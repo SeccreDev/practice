bf2ae34b08dd302a9212fba5614d0d9ff4e1e949
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-03-14 19:29:47 -0400

    Create hello.cpp

---
+++ c++/Hello World/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // Preprocessor Directive --> retrieves the header file. iostream contains code that is used to input/output (in this case cout)
+using namespace std; // Namespace is a statement that allows the program to access the standard entity (std)
+
+// Functions are group of programming statements. int - is a type of function that sends an integer value to the system.
+// Main function
+int main()
+{
+	// The line below outputs "Hello World!" to the screen. Thanks to the console output(cout) object.
+	cout << "Hello World!" << endl;
+
+	// Printing multiple lines
+	cout << "My name is SeccreDev" << endl;
+	cout << "I love c++" << endl;
+	cout << "That is why I am returning to it again :)\n";
+
+	//  Functions always return a value
+	return 0;
+}
