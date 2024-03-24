188992786d5044103357bf4a2a76d18976862b7a
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-03-23 22:09:00 -0400

    update main.cpp

--- C++/8- Unions/main.cpp
+++ C++/8- Unions/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+/// Unions - similar to a struct but all members share a single memory location and only 1 member of the union can be used at a time
+union WageInformation
+{
+	float hourlyRate;
+	double annualSalary;
+};
+
 int main()
 {
-  
-  return 0;
+	WageInformation wI;
+	wI.annualSalary = 29999.99;
+	wI.hourlyRate = 7.99;
+
+	cout << "Annual Salary: " << wI.annualSalary << endl;
+	cout << "Hourly Rate: " << wI.hourlyRate << endl;
+
+	return 0;
 }
