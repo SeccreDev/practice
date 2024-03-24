487800479fa9c0f5bc24ce8c4c609fb0947aadff
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-03-24 12:43:57 -0400

    update main.cpp

--- C++/9- Classes/main.cpp
+++ C++/9- Classes/main.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+using namespace std;
 
+class Date
+{
+    
+    private:
+       int day;
+       int month;
+       int year;
+    public:
+       Date(int day, int month, int year)
+       {
+        setDay(day);
+        setMonth(month);
+        setYear(year);
+       }
+
+       void setDay(int day)
+       {
+        day = this.day;
+       }
+
+       void setMonth(int month)
+       {
+        month = this.month;
+       }
+
+       void setYear(int year)
+       {
+        year = this.year;
+       }
+
+       int getDay()
+       {
+        return day;
+       }
+
+       int getMonth()
+       {
+        return month;
+       }
+
+       int getYear()
+       {
+        return year;
+       }
+
+}
