0a679d192376b50e70d8246965e1e07dc81bb344
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-03-26 22:42:10 -0400

    update Date.h

--- C++/9- Classes/Date.h
+++ C++/9- Classes/Date.h
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+/// OOP - based on objects that encapsulate the data and functions that work on them
 /// Class - a programmer-defined data type used to define objects.
 class Date
 {
@@ -11,16 +12,16 @@ class Date
     *                   public: can be accessed by functions outside of the class.
     
     */
-    // Member variables are usually private
+    // Attributes/Member variables are usually private
     private:
-        int day;
+        int day; 
         int month;
         int year;
 
     // Setters and getters functions are usually public
     public:
 
-        /// Member Functions - are part of the class definition, we can place the entire function definition inside the class (inline functions) or just the prototype (to be defined elsewhere)
+        /// Member Functions - procedures/functions that act on the attributes of the class. They are part of the class definition, we can place the entire function definition inside the class (inline functions) or just the prototype (to be defined elsewhere)
         // Constructor - member function used to initialize data members of a class. It must be public with the same name as the class and cannot have a return type
         /*Date() // Default constructor - takes no argument due to no parameters OR all parameters have default values. If a class has any programmer-defined constructors, it must have a programmer-defined default constructor
         {
@@ -54,21 +55,27 @@ class Date
 
 
         // Accesor/Getter function - uses, acceses a member variable but does not modify it
-        int getDay()
+        int getDay() const
         {
             return day;
         }
 
-        int getMonth()
+        int getMonth() const
         {
             return month;
         }
 
-        int getYear()
+        int getYear() const
         {
             return year;
         }
 
         // Functions Prototype, to be define elsewhere
         void printDate();
+
+        // Destructor - member function automatically called when an object is to be destroyed
+        ~Date()
+        {
+
+        }
 };
