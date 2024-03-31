f9d93547769d6444d1ab69d02300f0c4e61b3069
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-03-30 22:37:44 -0400

    update main.cpp

--- C++/11. Arrays/main.cpp
+++ C++/11. Arrays/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
@@ -26,6 +27,38 @@ int main()
 	{
 		cout << "Test score for test number " << i + 1 << ": " << tests[i] << endl;
 	}
+
+	// Getting array data from a file
+	int sales[SIZE];
+	ifstream dataFile("sales.dat");
+	if (!dataFile)
+		cout << "Error opening data file\n";
+	else
+	{
+		// Input from file to the array
+		for (int i = 0; i < SIZE; i++)
+		{
+			dataFile >> sales[i];
+		}
+		// Outputting the array's content
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << sales[i] << endl;
+		}
+		dataFile.close();
+	}
+
+	// No bound checking - there are no checks in c++ that an array subscript is in range. An invalid array subscript can cause program to overwrite other memory
+	int num[SIZE]; // Index is 0 to 9
+	//num[10] = 25; // Not valid
+
+	// Array initialization with a list
+	int numbers[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	// Partial Array Initialization
+	int numbers2[SIZE] = { 0, 20, 40 }; // Array is initialized with fewer values than the size declarator, remaining elements will be set to 0 or empty string
+	cout << numbers2[3]; // Outputs 0
+
 
 	return 0;
 }
