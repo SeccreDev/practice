3274ebb92128e7eaaf981ffe4cb9df92838583c5
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-04 21:01:50 -0400

    update Figure.h

--- C++/14. Array of Objects & Structures/Figure.h
+++ C++/14. Array of Objects & Structures/Figure.h
@@ -1,1 +1,20 @@
+#include <iostream>
+#ifndef FIGURE_H
+#define FIGURE_H
+using namespace std;
 
+class Figure
+{
+	private:
+		int side;
+
+	public:
+		Figure(int s = 10)
+		{
+			side = s;
+		}
+
+		int getSide();
+};
+
+#endif
