7cb1cad70cf5d65dbc0c16252eec33d51f35c19a
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-10 18:02:37 -0400

    update main.cpp

--- C++/17. Pointers/main.cpp
+++ C++/17. Pointers/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void getNumber(int *intPointer);
+void swapValues(int *a, int *b);
+void swapAddresses(int *&a, int *&b);
+
 int main()
 {
 	// Pointers - each variable in a program uses a unique location in memory that has an address. The address operator & is used to access the address of a variable:
 	int number = 17;
 	cout << &number << endl; // Print the hex address
+	cout << "===============================================" << endl;
 
     // Pointer variable (pointer) - a variable that holds, stores, the address of a memory location. Alternate way to access memory locations. 
 	//								Size of pointer for 32 bit the size is 4 bytes and for 64 bit the size is 8 bytes.
@@ -14,7 +19,113 @@ int main()
 	cout << intPtr << endl; // Print the hex address
 	cout << *intPtr << endl; // Prints the content of the address which is 17
 	*intPtr = 9; // Changes the content of the address from 17 to 9
-	cout << *intPtr;
+	cout << *intPtr << endl;
+	cout << number << endl;
+	cout << "===============================================" << endl;
 
+	// Pointers and Arrays
+	int values[3] = { 1, 2, 3 };
+	cout << values << endl; // will display the hex address of the first element
+	cout << values[0] << endl; // displays 1
+	cout << &values[0] << endl; // will display the hex address of the first element
+	cout << *values << endl; // will display 1. array name can be used as a constant pointer, so *values can be read as "the value being pointed by values"
+	int* valuesPointer = values; // Pointer can be used as an array name
+	cout << valuesPointer[2] << endl; // displays 3
+	cout << *(valuesPointer + 2) << endl; // displays 3. (address in valuesPointer) + (2 * size of int(this is done automatically))
+	cout << *(values + 2) << endl; // displays 3. (address in values) + (2 * size of int(this is done automatically))
+	cout << "===============================================" << endl;
+
+	// Array elements
+	cout << values[1] << endl; // Access by using []. Array notation
+	cout << *(values + 1) << endl; // Access by using subscript arithmetic. Pointer Notation
+	cout << valuesPointer[1] << endl; // Pointer to an array and []. Array notation
+	cout << *(valuesPointer + 1) << endl; // Pointer to an array and subscript arithmetic. Pointer Notation
+	cout << "===============================================" << endl;
+
+	// Pointer Arithmetic
+	cout << *valuesPointer++ << endl; // Points 1 then will go to the next address which is the address for int 2
+	cout << *valuesPointer-- << endl; // Outputs 2 then will go to the previous address which is the addres for int 1
+	cout << *valuesPointer << endl; // Outputs 1
+	cout << ++ * valuesPointer << endl; // Will go to the next address which is the address for int 2 and then will output 2
+	*valuesPointer -= 1; // Goes to the previous address which is the address for int 1
+	cout << *valuesPointer << endl;  // Outputs 1
+	valuesPointer += 2; // Skips to the last element
+	cout << *valuesPointer << endl; // Outputs 3
+	cout << "===============================================" << endl;
+
+	// Pointer Subtraction
+	cout << valuesPointer - values << endl; // Will output 2 because is the number of integers between valuesPointer and values
+	cout << "===============================================" << endl;
+
+	// Pointer Initialization
+	int* integerPointer = 0, * integerPointer2 = NULL; // Can be initialize to zero or null
+	int magicNumber, * magicPointer = &magicNumber;
+	cout << magicPointer << endl; // Output address of magic number
+	*magicPointer = 2; // sets the content of magic number by using a pointer
+	cout << *magicPointer << endl; // outputs the content of magic number by using a pointer
+	cout << magicNumber << endl; // outputs the content of magic number
+	cout << "===============================================" << endl;
+
+	// Pointer comparison
+	int value = 80, value2 = 40,
+		* intPointerValue = &value, *intPointerValue2 = &value2;
+
+	if (intPointerValue < intPointerValue2) // Compares the addresses
+	{
+		cout << "Comparing the addresses, intPointerValue2 has the bigger address" << endl;
+	}
+	else
+	{
+		cout << "Comparing the addresses, intPointerValue has the bigger address" << endl;
+	}
+
+	if (*intPointerValue < *intPointerValue2)
+	{
+		cout << "Comparing the values, intPointerValue2 has the bigger value" << endl;
+	}
+	else
+	{
+		cout << "Comparing the values, intPointerValue has the bigger value" << endl;
+	}
+	cout << "===============================================" << endl;
+
+	// Pointer in Functions - Pointers can be parameter and they work like reference parameter to allow changes to argument from within a function. Must be dereference to access content of the address
+	int number1, number2= 20, *intPointer1 = &number1, *intPointer2 = &number2;
+	getNumber(intPointer1);
+	cout << number1 << endl;
+	cout << "===============================================" << endl;
+	cout << "Swaping values..." << endl;
+	swapValues(intPointer1, intPointer2);
+	cout << "Number1: " << number1 << endl;
+	cout << "Number2: " << number2 << endl;
+	cout << "intPointer1: " << intPointer1 << " pointing to number1: " << *intPointer1 << endl;
+	cout << "intPointer2: " << intPointer2 << " pointing to number2: " << *intPointer2 << endl;
+	cout << "===============================================" << endl;
+	cout << "Swaping addresses..." << endl;
+	swapAddresses(intPointer1, intPointer2);
+	cout << "Number1: " << number1 << endl;
+	cout << "Number2: " << number2 << endl;
+	cout << "intPointer1: " << intPointer1 << " << pointing to number2 now: " << *intPointer1 << endl;
+	cout << "intPointer2: " << intPointer2 << " << pointing to number1 now: " << *intPointer2 << endl;
 	return 0;
+}
+
+void getNumber(int* intPointer)
+{
+	cout << "Input a number: ";
+	cin >> *intPointer;
+}
+
+void swapValues(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void swapAddresses(int *&a, int *&b)
+{
+	int *temp = a;
+	a = b;
+	b = temp;
 }
