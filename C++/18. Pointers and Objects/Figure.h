aa277ea233f6c4346f33e9c15b0db044f4417926
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-12 23:03:13 -0400

    update Figure.h

--- C++/18. Pointers and Objects/Figure.h
+++ C++/18. Pointers and Objects/Figure.h
@@ -1,1 +1,42 @@
+#include <iostream>
+#ifndef FIGURE_H
+#define FIGURE_H
+using namespace std;
 
+class Figure
+{
+	private:
+		int sides;
+		float area;
+
+	public:
+		Figure(int s = 0, float a = 0)
+		{
+			sides = s;
+			area = a;
+		}
+
+		// Getters
+		int getSide()
+		{
+			return sides;
+		}
+
+		float getArea()
+		{
+			return area;
+		}
+
+		// Setters
+		void setSide(int s)
+		{
+			sides = s;
+		}
+
+		void setArea(float a)
+		{
+			area = a;
+		}
+
+};
+#endif
