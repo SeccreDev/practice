5d63d704c610b2fad515a65b78ebf4095d17550a
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-17 19:02:08 -0400

    update main.cpp

--- C++/19. Classes Shenanigans/main.cpp
+++ C++/19. Classes Shenanigans/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include "Person.h"
+//#include "Animal.h"
 using namespace std;
 
 int main()
 {
 	// Static Member Variables can be accessed or modified by any object of the class, all modifications are visible to all objects of the class
-	// cout << Person::personCount; // cannot be accessed like this because it is a private member; otherwise it would output the personCount variable
 	Person p1, p2;
-	cout << p1.getPersonCount() << endl;
-	cout << p2.getPersonCount() << endl;
-	cout << Person::getPersonCount() << endl;
+	// Static Member Variables using an object and another one using the class scope resolution operator (Person::)
+	//cout << Person::personCount; // cannot be accessed like this because it is a private member; otherwise it would output the personCount variable
+	//cout << p1.personCount; // cannot be accessed like this because it is a private member; otherwise it would output the personCount variable
+	
+	// Calling Static Member Function
+	cout << p1.getPersonCount() << endl; // Calling the static member function from an object. Outputs 2
 	Person::incrementPersonCount();
-	cout << Person::getPersonCount() << endl;
+	cout << Person::getPersonCount() << endl; // Calling the static member function using the class scope resolution operator. Outputs 3
+	cout << p2.getPersonCount() << endl; // Calling the static member function from an object. Outputs 3
+	
+	cout << "===========================================\n";
+
+	// Friend function - calling
+	Person p3;
+	setMembers(p3, 50, "William");
+	cout << "Person 3 Name: " << p3.getName() << endl;
+	cout << "Person 3 Age: " << p3.getAge() << endl;
+	cout << "===========================================\n";
+	
+	// Friend class
+	Person popuri;
+	Animal dog;
+	popuri.setAnimal(dog, 15, "Lucy");
+	popuri.printAnimal(dog);
 
 	return 0;
 }
