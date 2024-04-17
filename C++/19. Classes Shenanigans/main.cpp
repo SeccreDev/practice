cd026c1968a53c002d52319747ccae6f2b18026c
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-16 22:34:51 -0400

    Update main.cpp

--- C++/19. Classes Shenanigans/main.cpp
+++ C++/19. Classes Shenanigans/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include "Person.cpp"
+#include "Person.h"
 using namespace std;
-
-// Static Member Variable must be defined outside of the class declaration
-//int Person::personCount = 0; // Static Member Variable initialized to 0
 
 int main()
 {
 	// Static Member Variables can be accessed or modified by any object of the class, all modifications are visible to all objects of the class
+	// cout << Person::personCount; // cannot be accessed like this because it is a private member; otherwise it would output the personCount variable
 	Person p1, p2;
-	cout << p1.getPersonCount();
-
+	cout << p1.getPersonCount() << endl;
+	cout << p2.getPersonCount() << endl;
+	cout << Person::getPersonCount();
 
 	return 0;
 }
-
