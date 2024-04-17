b24e2b3aa2922b1ca01c713559a921d1132fb2f2
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-17 19:04:34 -0400

    Create Animal.h

---
+++ C++/19. Classes Shenanigans/Animal.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#ifndef ANIMAL_H
+#define ANIMAL_H
+using namespace std;
+
+class Animal
+{
+private:
+	int animalAge;
+	string animalName;
+	
+	// An entire class can be declared a friend of a class
+	friend class Person; // All member functions of Person have unrestricted access to all members of Animal, including the private members. This is not recommended.
+	                     // Friendship should only be used with functions that MUST have access to the private members of a class
+
+public:
+	/// Constructor
+	// this Pointer - can be used to access members that may be hidden by parameters with the same name
+	Animal(string name = "", int age = 0)
+	{
+		animalName = name; 
+		animalAge = age; 
+	}
+
+	// Setters
+	void setAnimalAge(const int a)
+	{
+		animalAge = a;
+	}
+	void setAnimalName(const string n)
+	{
+		animalName = n;
+	}
+
+	// Getters
+	int getAnimalAge() const
+	{
+		return animalAge;
+	}
+
+	string getAnimalName() const
+	{
+		return animalName;
+	}
+};
+#endif
