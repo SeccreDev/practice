c497efb1afbd5a84b212402acc8acc23a08afa02
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-18 23:48:53 -0400

    update Copy.h

--- C++/19. Classes Shenanigans/Copy.h
+++ C++/19. Classes Shenanigans/Copy.h
@@ -4,25 +4,54 @@
 #define COPY_H
 using namespace std;
 
-// Copy Constructor - a special constructor used when a newly created object is initialized to the data of another object of the same class.
-//                    If the programmer does not define a copy constructor for the class then the compiler calls a default copy constructor which copies corresponding
-//					  data from one object to another of the same class. Default copy constructor copies field-to-field values, using member-wise assignment.
-class Copy
+// This class will use a Default Copy Constructor as it does not have one define
+class BadCopy
 {
 	private:
 		float *floatPointer;
 
 	public:
-		Copy(int x = 0)
+		BadCopy(int x = 0)
 		{
 			floatPointer = new float;
 			*floatPointer = x; // Problem occur when object contain pointers to dynamic storage
 		}
 
-		~Copy()
+		~BadCopy()
 		{
 			delete floatPointer;
 		}
+
+};
+
+// This class will use the Copy Constructor define by the programmer
+class GoodCopy
+{
+private:
+	float *floatPointer;
+
+public:
+	// Programmer-Defined Copy Constructor - Takes one parameter: an object of the same class by reference. The copy constructor uses the data in the object passed as
+	//                                       as parameter to initialize the object being created. Reference parameter should be const to avoid corruption. This copy constructor
+	//                                       will avoid problems caused by memory sharing. 
+	// 1- Should allocate separate memory to hold new object's dynamic member data.
+	// 2- Should make new object's pointer point to this memory
+	// 3- Should copy the data, not the pointer, from the original object to the new object
+	GoodCopy(const GoodCopy &obj)
+	{
+		floatPointer = new float;
+		*floatPointer = *obj.floatPointer;
+	}
+	GoodCopy(float x = 0)
+	{
+		floatPointer = new float;
+		*floatPointer = x; 
+	}
+
+	~GoodCopy()
+	{
+		delete floatPointer;
+	}
 
 };
 #endif
