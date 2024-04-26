df8b35115e285ea11e4cf3c64607bc2e12c14075
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-04-25 23:06:03 -0400

    created Inheritance.h

---
+++ C++/19. Classes Shenanigans/Inheritance.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#ifndef INHERITANCE_H
+#define INHERITANCE_H
+class Child
+{
+	private:
+		string name;
+
+	public:
+		Child(string name)
+		{
+			this->name = name;
+		}
+
+};
+#endif
+
