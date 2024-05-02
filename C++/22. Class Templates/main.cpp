7dce4d889f5636e4e6c8b97c9bd06731fac87892
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-01 20:38:43 -0400

    update main.cpp

--- C++/22. Class Templates/main.cpp
+++ C++/22. Class Templates/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "ClassTemp.h"
 using namespace std;
 
 int main()
 {
-  
-  return 0;
+	// To create an object of a class defined by a template, specify the actual parameters for the formal data types
+	ClassTemp<int> intAdd;
+	ClassTemp<string> stringAdd;
+	cout << intAdd.add(5, 8) << endl;
+	cout << stringAdd.add("Maria", " Luis") << endl;
+
+	return 0;
 }
