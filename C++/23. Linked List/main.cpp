a49add4e0c6ba9d0f988712fd90e3d5df3d5b90a
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-01 22:56:50 -0400

    Create main.cpp

---
+++ C++/23. Linked List/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Linked List - a sequence of data structures nodes; each node containing data and a pointer to its successor node, serving as a link. The last node in the list
+//               has its successor pointer set to NULL. The node at the beginning is called 'head of the list', and its just a pointer to a node without data.
+//               The entire list is identified by the pointer to the first node, the pointer is called the head. The list head may have the address of the first node,
+//               or may be NULL. The nodes of a linked list are usually dynamically allocated. Nodes can be added or removed from the linked list during execution.
+//               Addition or removal of nodes can take place at beginning, middle or, end of the list
+int main()
+{
+	return 0;
+}
