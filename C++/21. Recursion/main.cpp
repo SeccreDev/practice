82a6d76d6ac41b0a7a77fced43a9918a6c284863
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-01 20:14:08 -0400

    update main.cpp

--- C++/21. Recursion/main.cpp
+++ C++/21. Recursion/main.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 // Recursive function - a function that calls itself. Is a loop that can be useful in solving problems that can be broken down into smaller sub-problems of the same type.
 //						a base case should eventually be reached, at that time the recursion will stop.
+// Types of Recursion: -Direct Recursion: a function calls itself
+//					   -Indirect Recursion: function A calls function B, function B calls function A.
+
 void hello() // This fumction calls itself infinitely, a base case is never reached.
 {
 	cout << "Hello";
@@ -25,4 +28,82 @@ void countDown(int number)
 	}
 }
 
-// Types of Recursion: -Direct Recursion: a function calls itself       -Indirect Recursion: function A calls function B, function B calls function A.
+// Finding Factorial by using a Recursive Function
+int factorial(int number)
+{
+	if (number == 0) // Base case
+	{
+		return 1;
+	}
+	else
+	{
+		return number * factorial(number - 1); // Recursive Call
+	}
+}
+
+// Finding Greatest Common Divisor by using a Recursive Function
+int gcd(int x, int y)
+{
+	if (x % y == 0) // Base case
+	{
+		return y;
+	}
+	else
+	{
+		return gcd(y, x % y); // Recursive Call
+	}
+}
+
+// Using a Recursive Function for Fibonacci Sequence
+int fibonacci(int number)
+{
+	if (number <= 0) // Base case
+	{
+		return 0;
+	}
+	else if (number == 1) // Base case
+	{
+		return 1;
+	}
+	else
+	{
+		return fibonacci(number - 1) + fibonacci(number - 2); // Recursive Call
+	}
+}
+
+// Binary Search by using a Recursive Function
+int recursiveBinarySearch(int array[], int low, int high, int number)
+{
+	int mid = (low + high) / 2;
+	if (low > high) // Base case
+	{
+		return -1;
+	}
+	if (array[mid] == number) // Base case
+	{
+		return mid;
+	}
+	if (array[mid] > number)
+	{
+		return recursiveBinarySearch(array, low, mid - 1, number);
+	}
+	else
+	{
+		return recursiveBinarySearch(array, mid + 1, high, number);
+	}
+}
+
+int main()
+{
+	//hello(); // infinite
+	countDown(30);
+	cout << "========================\n";
+	cout << factorial(10) << endl;
+	cout << "========================\n";
+	cout << gcd(10, 5) << endl;
+	cout << "========================\n";
+	cout << fibonacci(30) << endl;
+	cout << "========================\n";
+
+	return 0;
+}
