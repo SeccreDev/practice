9b9fcf8098b3121e09ac4f0ac6ca209b621db06b
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-09-24 19:01:11 -0400

    Update main2.cpp

--- C++/18. Pointers and Objects/main2.cpp
+++ C++/18. Pointers and Objects/main2.cpp
@@ -13,9 +13,11 @@ int main()
 	int number = 25;
 	theStruct.size = 5;
 	theStruct.somePointer = &number;
-	
+
+	// Struct pointer needs to be dereferenced to show the content
 	cout << "The contents of somePointer is: " << *(theStruct.somePointer) << endl;
-	
+
+	// Creating a dynamically structure 
 	StructWithPointer *structP = new StructWithPointer;
 	structP->somePointer = &number;
 	cout << "The contents of somePointer within structP is: " << *(structP->somePointer) << endl;
