648f92a39421ab1390d8c314da3d24598ff8d4ce
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-09-25 17:08:24 -0400

    Update Person.h

--- C++/19. Classes Shenanigans/Person.h
+++ C++/19. Classes Shenanigans/Person.h
@@ -8,7 +8,7 @@ using namespace std;
 class Person
 {
 	private:
-		// Static Member Variable - one instance of variable for the entire class (it iss shared by all objects of the class).
+		// Static Member Variable - one instance of variable for the entire class (it is shared by all objects of the class).
 		//                          can be initialized, used, before any class objects are created. Can be used to accumulate values from
 		//                          a number of different objects, or to count the number of objects instantiated...
 		static int personCount; // Static Member Variable must be declared in class with the keyword static
@@ -28,8 +28,8 @@ class Person
 
 		/// Setters
 		// Constant Parameter  - when const is in parameter list the function is prevented from modifying the parameter. The parameter is read-only
-		void setAge(const int a);
-		void setName(const string n);
+		void setAge(const int &a);
+		void setName(const string &n);
 
 		// Getters
 		// Constant Member Functions - the function is prevented from modifying the object. None of the objects data members can be modified. Recommended for all getters
