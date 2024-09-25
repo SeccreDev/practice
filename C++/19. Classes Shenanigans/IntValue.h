b9433e0ff2e931ff35486c2af4ecce92a34e5cba
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-09-25 16:12:02 -0400

    Update IntValue.h

--- C++/19. Classes Shenanigans/IntValue.h
+++ C++/19. Classes Shenanigans/IntValue.h
@@ -15,8 +15,8 @@ class IntValue
 		}
 
 		// Conversion Operators - Member functions that tell the compiler how to convert an object of the class type to a value of any other data type.
-		//						  The conversion information provided by the conversion operators is automatically used by the compiler in assignments, initializations,
-		//						  and parameter passing. Conversion operator must be a member function of the class you are converting from. The name of the operator is the
+		//			  The conversion information provided by the conversion operators is automatically used by the compiler in assignments, initializations,
+		//			  and parameter passing. Conversion operator must be a member function of the class you are converting from. The name of the operator is the
 		//                        name of the type you are converting to. The operatoor does not specify a return type.
 		operator int()
 		{
