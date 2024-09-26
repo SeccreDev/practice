1eb4ae5fb4dd4e2f6d0a9dd4948a2fe2afe6f936
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-09-26 16:35:42 -0400

    Update ClassTemp.h

--- C++/22. Class Templates/ClassTemp.h
+++ C++/22. Class Templates/ClassTemp.h
@@ -3,7 +3,7 @@
 
 // Class template - can be used whenever we need several classes that only differs in the type of some of their data members, or in the type of the parameters of ther member functions.
 // Templates can be combined with inheritance. You can derive: -Non template classes from a template class: instantiate the base class template and then inherit from it
-//															   -Template class from a template class.
+//							       -Template class from a template class.
 template <class T>
 class ClassTemp
 {
